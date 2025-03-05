632b9196ad1cec2d7787c814bd8f37ab0a2a3983
Victor Alencar <victor.alencar@startleftsecurity.com>
2025-03-05 19:00:14 -0300

    Creating global exception filter to avoid stack traces on the response

---
+++ Filters/GlobalExceptionFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace GameOfLifeApi.Filters;
+
+public class GlobalExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        // e.g., _logger.LogError(context.Exception, "Unhandled exception occurred.");
+
+        var response = new
+        {
+            Message = "An unexpected error occurred. Please try again later."
+        };
+
+        context.Result = new ObjectResult(response)
+        {
+            StatusCode = 500
+        };
+
+        context.ExceptionHandled = true;
+    }
+}
