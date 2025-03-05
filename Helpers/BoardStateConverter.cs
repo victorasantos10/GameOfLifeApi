5204c6cc061834049dcf63ca185e2489e031b389
Victor Alencar <victor.alencar@startleftsecurity.com>
2025-03-05 18:41:08 -0300

    Converter for serializing/deserializing JSON

---
+++ Helpers/BoardStateConverter.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json;
+
+namespace GameOfLifeApi.Helpers
+{
+    public static class BoardStateConverter
+    {
+        public static string Serialize(bool[][] board) => JsonSerializer.Serialize(board);
+        public static bool[][]? Deserialize(string json) => JsonSerializer.Deserialize<bool[][]>(json);
+    }
+}
