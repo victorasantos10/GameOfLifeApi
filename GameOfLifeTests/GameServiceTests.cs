62f1950a8d3bb7a4e039334863a3c6fd40dcc6bb
Victor Alencar <victoralencar_stos@hotmail.com>
2025-03-06 17:16:46 -0300

    moving git repo one level up, so we can have both tests and api folders on the repo

---
+++ GameOfLifeTests/GameServiceTests.cs
@@ -0,0 +1,165 @@
+using GameOfLifeApi.Helpers;
+using GameOfLifeApi.Models;
+using GameOfLifeApi.Repositories;
+using GameOfLifeApi.Services;
+using Moq;
+
+namespace GameOfLifeTests
+{
+    public class GameServiceTests
+    {
+        private readonly Mock<IBoardRepository> _boardRepositoryMock;
+        private readonly GameService _service;
+
+        public GameServiceTests()
+        {
+            _boardRepositoryMock = new Mock<IBoardRepository>();
+            _service = new GameService(_boardRepositoryMock.Object);
+        }
+
+        [Fact]
+        public async Task CreateBoardAsync_ValidBoard_ReturnsNewGuid()
+        {
+            bool[][] boardState = new bool[][]
+            {
+                new bool[] { true, false, true },
+                new bool[] { false, true, false },
+                new bool[] { true, false, true }
+            };
+
+            _boardRepositoryMock.Setup(r => r.AddBoardAsync(It.IsAny<Board>()))
+                .Returns(Task.CompletedTask);
+
+            var boardId = await _service.CreateBoardAsync(boardState);
+
+            Assert.NotEqual(Guid.Empty, boardId);
+            _boardRepositoryMock.Verify(r => r.AddBoardAsync(It.Is<Board>(b => b.Id == boardId.Value)), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetNextStateAsync_ValidBoard_ReturnsNextState()
+        {
+            bool[][] boardState = new bool[][]
+            {
+                new bool[] { false, false },
+                new bool[] { false, false }
+            };
+
+            var board = new Board
+            {
+                Id = Guid.NewGuid(),
+                State = BoardStateConverter.Serialize(boardState),
+                CreatedAt = DateTime.UtcNow,
+                LastUpdated = DateTime.UtcNow
+            };
+
+            _boardRepositoryMock.Setup(r => r.GetBoardByIdAsync(board.Id))
+                .ReturnsAsync(board);
+            _boardRepositoryMock.Setup(r => r.UpdateBoardAsync(It.IsAny<Board>()))
+                .Returns(Task.CompletedTask);
+
+            var nextState = await _service.GetNextStateAsync(board.Id);
+
+            Assert.Equal(boardState.Length, nextState.Value.Length);
+            for (int i = 0; i < boardState.Length; i++)
+            {
+                Assert.Equal(boardState[i], nextState.Value[i]);
+            }
+        }
+
+        [Fact]
+        public async Task GetNextStateAsync_InvalidBoardId_ThrowsArgumentException()
+        {
+            Guid invalidId = Guid.NewGuid();
+            _boardRepositoryMock.Setup(r => r.GetBoardByIdAsync(invalidId))
+                .ReturnsAsync((Board)null);
+
+            var result = await _service.GetNextStateAsync(invalidId);
+            Assert.False(result.IsSuccess);
+        }
+
+        [Fact]
+        public async Task GetStateAfterStepsAsync_InvalidSteps_ThrowsArgumentException()
+        {
+            Guid boardId = Guid.NewGuid();
+
+            var result = await _service.GetStateAfterStepsAsync(boardId, 0);
+            Assert.False(result.IsSuccess);
+        }
+
+        [Fact]
+        public async Task GetStateAfterStepsAsync_ValidBoard_ReturnsStateAfterSteps()
+        {
+            bool[][] boardState = new bool[][]
+            {
+                new bool[] { false, false, false },
+                new bool[] { false, false, false },
+                new bool[] { false, false, false }
+            };
+
+            var board = new Board
+            {
+                Id = Guid.NewGuid(),
+                State = BoardStateConverter.Serialize(boardState),
+                CreatedAt = DateTime.UtcNow,
+                LastUpdated = DateTime.UtcNow
+            };
+
+            _boardRepositoryMock.Setup(r => r.GetBoardByIdAsync(board.Id))
+                .ReturnsAsync(board);
+            _boardRepositoryMock.Setup(r => r.UpdateBoardAsync(It.IsAny<Board>()))
+                .Returns(Task.CompletedTask);
+
+            var stateAfterSteps = await _service.GetStateAfterStepsAsync(board.Id, 5);
+
+            Assert.Equal(boardState.Length, stateAfterSteps.Value.Length);
+            for (int i = 0; i < boardState.Length; i++)
+            {
+                Assert.Equal(boardState[i], stateAfterSteps.Value[i]);
+            }
+        }
+
+        [Fact]
+        public async Task GetFinalStateAsync_StableBoard_ReturnsFinalState()
+        {
+            bool[][] boardState = new bool[][]
+            {
+                new bool[] { false, false, false },
+                new bool[] { false, false, false },
+                new bool[] { false, false, false }
+            };
+
+            var board = new Board
+            {
+                Id = Guid.NewGuid(),
+                State = BoardStateConverter.Serialize(boardState),
+                CreatedAt = DateTime.UtcNow,
+                LastUpdated = DateTime.UtcNow
+            };
+
+            _boardRepositoryMock.Setup(r => r.GetBoardByIdAsync(board.Id))
+                .ReturnsAsync(board);
+            _boardRepositoryMock.Setup(r => r.UpdateBoardAsync(It.IsAny<Board>()))
+                .Returns(Task.CompletedTask);
+
+            var finalState = await _service.GetFinalStateAsync(board.Id);
+
+            Assert.Equal(boardState.Length, finalState.Value.Length);
+            for (int i = 0; i < boardState.Length; i++)
+            {
+                Assert.Equal(boardState[i], finalState.Value[i]);
+            }
+        }
+
+        [Fact]
+        public async Task GetFinalStateAsync_BoardNotFound_ThrowsArgumentException()
+        {
+            Guid boardId = Guid.NewGuid();
+            _boardRepositoryMock.Setup(r => r.GetBoardByIdAsync(boardId))
+                .ReturnsAsync((Board?)null);
+
+            var result = await _service.GetFinalStateAsync(boardId);
+            Assert.False(result.IsSuccess);
+        }
+    }
+}
